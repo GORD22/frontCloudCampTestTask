4779892446b7085a141ccd7f01c56eedf0488050
GORD22 <andreyanov.15@mail.ru>
2023-06-18 13:56:17 +0700

    settings for deploy

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,11 @@ import style from './assets/style/App.module.scss';
 import { ExtendedForm } from './components/ExtendedForm';
 import { InitialForm } from './components/InitialForm';
 
-const App: FC<{}> = () => {
+const App = () => {
 
   return (
     <div className={style.siteContainer}>
       <InitialForm />
-      <Routes>
-        <Route path={'/'} element={<Navigate to={'/start'} />} />
-        <Route path={'/start'} element={<InitialForm />} />
-        <Route path={'/create'} element={<ExtendedForm />} />
-      </Routes>
     </div>
   );
 }
