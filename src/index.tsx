e1d9c32736de1e35b658a4a04ec38df786dc6c36
GORD22 <andreyanov.15@mail.ru>
2023-06-18 13:52:13 +0700

    settings for deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './assets/style/index.scss';
-import { Provider } from 'react-redux';
 import { store } from './store/store';
-import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,11 +12,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <HashRouter basename='/start'>
         <Provider store={store}>
           <App />
         </Provider>
-      </HashRouter>
     </BrowserRouter>
   </React.StrictMode>
 );
